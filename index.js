ecfa08e1320136ad239bf9756786e413222de289
Angel Rego <angelrego@gmail.com>
2021-07-29 13:48:33 +0200

    execute: allow either name or raw string algorithm

--- index.js
+++ index.js
@@ -32,12 +32,18 @@ export async function get(name) {
 
 /**
  * Execute an algorithm
- * @param {string} name - Example: bell or bell.qasm or bell.quil 
+ * @param {string} name_or_data - Example: bell or bell.qasm or bell.quil. If data, the algorithm provided is executed without saving in the repo. 
  * @returns string
  */
-export async function execute(name) {
-  const realName = name.includes('.') ? name : name + EXT
-  const res = await fetch(ENDPOINT_SERVICES + '/run/' + realName, { method: 'post' })
-  if (!res.ok) throw res.status
-  return await res.text()
+export async function execute(name_or_data) {
+  if (name_or_data.length < 20) {
+    const realName = name_or_data.includes('.') ? name_or_data : name_or_data + EXT
+    const res = await fetch(ENDPOINT_SERVICES + '/run/' + realName, { method: 'post' })
+    if (!res.ok) throw res.status
+    return await res.text()
+  } else {
+    const res = await fetch(ENDPOINT_SERVICES + '/run/', { method: 'post', body: name_or_data })
+    if (!res.ok) throw res.status
+    return await res.text()
+  }
 }
